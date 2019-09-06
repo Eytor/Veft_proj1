0985658cccae4440cdbf4d4d15130d058b016a5d
eytor <eythoras16@ru.is>
2019-09-06 14:37:55 +0000

    added more data

--- TechnicalRadiation.Repositories/Data/DataProvider.cs
+++ TechnicalRadiation.Repositories/Data/DataProvider.cs
@@ -12,8 +12,283 @@ namespace TechnicalRadiation.Repositories.Data
       {
           Id = 1,
           Title = "news",
-          ShortDescription = "",
-          LongDescription ="",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 2,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 3,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 4,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 5,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 6,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 7,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 8,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 9,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 10,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 11,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 12,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 13,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 14,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 15,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 16,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 17,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 18,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 19,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 20,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 21,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 22,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 23,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 24,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 25,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
+          PublishDate = DateTime.Now,
+          ModifiedBy = "TechnicalRadiationAdmin",
+          CreatedDate = DateTime.Now,
+          ModifiedDate = DateTime.Now
+      },
+      new NewsItem
+      {
+          Id = 26,
+          Title = "news",
+          ShortDescription = "A short description of the news article",
+          LongDescription ="A full description of the news article",
           PublishDate = DateTime.Now,
           ModifiedBy = "TechnicalRadiationAdmin",
           CreatedDate = DateTime.Now,
