099d5e2523aa05f581c1a1fa2c2df9ea8f4239dd
eytor <eythoras16@ru.is>
2019-09-10 17:52:32 +0000

    added some code to controller

--- TechnicalRadiation/Controllers/ValuesController.cs
+++ TechnicalRadiation/Controllers/ValuesController.cs
@@ -22,7 +22,7 @@ namespace TechnicalRadiation.Controllers
 
         public ValuesController(IConfiguration config, IAuthentication Authentication)
         {
-            Authentication = Authentication;
+            this.Authentication = Authentication;
             Config = config;
         }
 
@@ -83,7 +83,6 @@ namespace TechnicalRadiation.Controllers
         }
 
         // The dotnet should have already new-ed a news file which I received from body.
-<<<<<<< HEAD
         // [HttpGet]
         // [Route("/authors/{authorID}")]
         // public ActionResult<string> Post([FromHeader] string xApiKey, [FromBody] TechnicalRadiation.Models.NewsItem news)
@@ -204,127 +203,5 @@ namespace TechnicalRadiation.Controllers
         // {
         //     return NoContent();
         // }
-=======
-        [HttpGet]
-        [Route("/authors/{authorID}")]
-        public ActionResult<string> Post([FromHeader] string xApiKey, [FromBody] TechnicalRadiation.Models.NewsItem news)
-        {
-            if (Authentication.Authenticate(xApiKey) == false)
-            {
-                return "Please provide authentication.";
-            }
-            // Take news and save in db if you have one
-            // var news = new NewsItem();
-            // Fill inn parameters
-            // news.name = "";
-            TechnicalRadiation.Models.NewsItem.save(news);
-            // DBNull.save(news);
-            return Created();
-            // return $"{authorID} Authenticated";
-        }
-
-        [HttpGet]
-        [Route("/authors/{authorID}/newsItem")]
-        public ActionResult<string> Post([FromHeader]int id, [FromHeader]string xApiKey, [FromBody] TechnicalRadiation.Models.NewsItem news)
-        {
-            if (Authentication.Authenticate(xApiKey) == false)
-            {
-                return "Please provide authentication.";
-            }
-
-            return Created();
-            // return $"{authorID} Authenticated";
-        }
-
-
-
-        // POST api/
-        [Route("")]
-        [HttpPost]
-        public IActionResult CreateNewNewsItem([FromBody] string value)
-        {
-            return Ok();
-        }
-
-        // PUT api/5
-        [HttpPut("{id:int}")]
-        public IActionResult UpdateNewsItem(int id, [FromBody] string value)
-        {
-            return NoContent();
-        }
-
-        [Route("/{newsitemId}")] 
-        [HttpDelete]
-        public ActionResult Delete([FromHeader]int id, [FromHeader]string xApiKey)
-        {
-            var delId = _technicalRadiationService.GetNewsById(id);
-            if(_technicalRadiationService.GetNewsById(id)){
-                technicalRadiationService.Delete(id);
-            }
-        }
-
-
-        [Route("/categories/{categoryId}")] 
-        [HttpDelete]
-        public ActionResult Delete([FromHeader]int cId, [FromHeader]string xApiKey)
-        {
-            var delId = _technicalRadiationService.GetNewsById(cId);
-            if(_technicalRadiationService.GetNewsById(id)){
-                technicalRadiationService.Delete(id);
-            }
-        }
-
-
-        // POST api/categories
-        [Route("categories")]
-        [HttpPost]
-        public IActionResult CreateNewCategory([FromBody] string value)
-        {
-            return Ok();
-        }
-
-        // PUT api/categories/5
-        [HttpPut("categories/{id:int}")]
-        public IActionResult UpdateCategory(int id, [FromBody] string value)
-        {
-            return NoContent();
-        }
-
-        // DELETE api/categories/5
-        [HttpDelete("categories/{id:int}")]
-        public IActionResult DeleteCategory(int id)
-        {
-            return Ok();
-        }
-
-        // POST api/authors
-        [Route("authors")]
-        [HttpPost]
-        public IActionResult CreateNewAuthor([FromBody] string value)
-        {
-            return Ok();
-        }
-
-        // PUT api/authors/5
-        [HttpPut("authors/{id:int}")]
-        public IActionResult UpdateAuthor(int id, [FromBody] string value)
-        {
-            return NoContent();
-        }
-
-        // DELETE api/authors/5
-        [HttpDelete("authors/{id:int}")]
-        public IActionResult DeleteAuthor(int id)
-        {
-            return Ok();
-        }
-
-        // PUT api/authors/5/newsItems/10
-        [HttpPut("authors/{authorId:int}/newsItems/{newsItemId:int}")]
-        public IActionResult LinkAuthorToNews(int authorId, int newsItemId)
-        {
-            return NoContent();
-        }
->>>>>>> 4a2150f811364b7565b1e911961e0920ec8bd97d
     }
 }
